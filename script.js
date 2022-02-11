16709135642e75cb6a3fec0c340ae9a42ce22908
julie ugolotti <julie.ugolotti@gmail.com>
2022-02-11 15:23:51 +0100

    exercise api

---
+++ script.js
@@ -0,0 +1,18 @@
+fetch("https://quotes15.p.rapidapi.com/quotes/random/", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "0435317791msh8166e3e33063498p199507jsn237f11e64307",
+		"x-rapidapi-host": "quotes15.p.rapidapi.com"
+	}
+})
+.then(response => response.json())
+.then(response => {
+    console.log(response);
+	console.log(response.content);
+    
+    document.getElementById('quote').innerHTML = response.content;
+    document.getElementById('author').innerHTML = '- ' + response.originator.name + ' -';
+})
+.catch(err => {
+	console.log(err);
+});
